db96066cab052f2b518cf529d42798094441744d
thanhdat21103 <128039058+thanhdat21103@users.noreply.github.com>
2023-04-20 22:43:38 +0700

    create footer and header
    create footer and header

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,26 @@
+@yield('main-section')
+<div id="footer">
+        <h2 class="row-heading">CONTACTS</h2>
+        <div class="row">
+            <div class="col col-half khoangcachdong">
+                <h2 class="heading">Keyboard shop</h2>
+                <p><i class="ti-location-pin"></i>118 Nguyen Khanh Toan - Cau Giay - Ha Noi</p>
+                <p><i class="ti-mobile"></i>+84 354676215</p>
+                <p><i class="ti-email"></i>truonganhtu190303@gmail.com</p>
+            </div>
+            <div class="col col-half">
+            <h2 class="heading2">Promotion Information</h2>
+                <input type="email" class="input-email" placeholder="Nhập email tại đây" style="padding-left: 10px;">
+                <button class="btn-email">Register</button>
+                <div class="social-btns">
+                    <a class="facebook" href="https://www.facebook.com/"><i class="ti-facebook"></i></a>
+                    <a class="youtube" href="https://www.youtube.com/"><i class="ti-youtube"></i></a>
+                    <a class="instagram" href="https://www.shopee.vn/"><i class="ti-instagram"></i></a>
+                    <a class="shopee" href="https://www.shopee.vn/"><i class="ti-shopping-cart"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="banquyen">
+            <div class="copyright">Bản quyền thuộc về Trương Anh Tú</div>
+        </div>
+    </div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('assets/css/giaodien.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/icon/themify-icons/themify-icons.css')}}">
+    
+</head>
+<body>
+    
+    <div class = "header">
+        <div id="header">
+            <!--begin-->
+                <ul id="nav">
+                    <li><a href="{{asset('home')}}">Home</a></li>
+                    <li><a href="#" onclick="smoothScroll(document.getElementById('introduction'))">Introduction</a></li>
+                    <li><a href="#" onclick="smoothScroll(document.getElementById('cartegory'))">Products</a></li>
+                    <li><a href="#" onclick="smoothScroll(document.getElementById('footer'))">Contacts</a></li>
+                </ul>
+                <!--end nav-->
+                <!--begin search button-->
+                <div class="btn row">
+                    <div class="btn_searching row">
+                    <form action="./hienthi_search.php" method="post">
+                        <input type="text" name="searching_name">
+                        <button type ="submit"><i class="ti-search"></i></input>
+                    </form>
+                    </div>
+                    <div class="btn_user">
+                    <a href="{{asset('artist')}}"><i class="search-icon ti-user"></i></a>
+                    </div>
+                    <div class="btn_giohang">
+                    <a href="./giohang.php"><i class="shopping-icon ti-shopping-cart-full"></i></a>
+                    </div>
+                    <div class="btn_logout">
+                    <a href="./logout.php"><i class="ti-share logout-icon"></i></a>
+                    </div>
+                    
+                </div>
+            </div>
+    </div>
+@yield('main-content')
