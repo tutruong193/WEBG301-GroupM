6e8f3da22f59927524f5ecbab1bc2876c0ed7c64
thanhdat21103 <128039058+thanhdat21103@users.noreply.github.com>
2023-04-20 23:12:05 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends('header')
 @section('main-section')
-
- 
+<h1>t</h1>
+ @foreach ($artists as $artist)
+ <h1>{{$artist -> FirstName}}</h1>
+ @endforeach
    
 @endsection
 @extends ('footer')
