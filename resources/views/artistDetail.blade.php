1bc89abe20229d39348fd83bdacb5f34b9720f92
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2023-04-21 22:41:27 +0700

    css and back-end artistDetail

--- resources/views/artistDetail.blade.php
+++ resources/views/artistDetail.blade.php
@@ -1,4 +1,4 @@
-<div class="details-product">
+<!-- <div class="details-product">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="rows row-width">
             <form enctype="multipart/form-data" id="add-to-cart-form" action="/cart/add" method="post" class="row form-width form-inline">
@@ -83,20 +83,7 @@
                            
                         </span> 
                         
-                        
-                    </div>
-                        <div class="form-group form_button_details">
-                            <header class="not_bg">Hide Hours</header>
-                            <div class="custom input_number_product custom-btn-number form-control">	
-                                <button class="btn_num num_2 button button_qty" onclick="var result = document.getElementById('qtym'); var qtypro = result.value; if( !isNaN( qtypro )) result.value++;return false;" type="button">+</button>
-                                <button class="btn_num num_1 button button_qty" onclick="var result = document.getElementById('qtym'); var qtypro = result.value; if( !isNaN( qtypro ) &amp;&amp; qtypro > 1 ) result.value--;return false;" type="button">-</button>
-                                <input type="text" id="qtym" name="quantity" value="1" class="form-control prd_quantity" onkeypress="if ( isNaN(this.value + String.fromCharCode(event.keyCode) )) return false;" onchange="if(this.value == 0)this.value=1;">
-                            </div>                                                               
-                            <button type="submit" class="btn btn-lg  btn-cart button_cart_buy_enable add_to_cart btn_buy">
-                                <span>Add to list</span>
-                            </button>									                          
-                        </div>
-                    </div>                   
+                                 
                    
                                                 
                 </div>
@@ -114,12 +101,12 @@
 <link href="vn" rel="stylesheet" type="text/css" media="all">
 <div class="app_buyx_promotion"></div>
 </form>
-            <!-- Tab -->
+
             <div class="tab_width_full">
                 <div class="row xs-margin-top-15">
                     
                     <div id="tab_ord" class="col-xs-12 col-sm-12 col-lg-12 col-md-12">
-                        <!-- Nav tabs -->
+
                         <div class="product-tab e-tabs not-dqtab">
                             <span class="border-dashed-tab"></span>
                             <ul class="tabs tabs-title clearfix">	
@@ -158,7 +145,52 @@
 
                 </div>
             </div>
-            <!-- Endtab -->
+
         </div>
     </div>
-</div>
+</div> -->
+@extends('header')
+@section('main-section')
+<header>
+    <link rel="stylesheet" href="{{asset('css/sanpham.css')}}">
+</header>
+<div class="sanpham">
+        <div class="container">
+            <div class="product-top row">
+                <p>Home</p><i class="ti-angle-right"></i>
+                <p>{{$artist -> FirstName}} {{$artist -> LastName}}</p>
+            </div>
+            <div class="product-content row">
+                <div class="product-content-left row">
+                    <div class="product-content-left-big-img">
+                        <img src="{{$artist -> Img}}" alt="">
+
+                    </div>
+                </div>
+                <div class="product-content-right">
+                    <form action="" method="POST">
+                        <div class="product-content-right-product-name">
+                            <p style="font-size:20px;color:black;font-weight:bold;">{{$artist -> FirstName}} {{$artist -> LastName}}</p>
+                        </div>
+                        <div class="product-content-right-product-price">
+                            <p>{{$artist -> BirthDate}}</p>
+                        </div>
+                        <div class="product-content-right-product-quantity">
+                            <p>{{$artist -> Description}}</p> 
+                        </div>
+                        <div class="product-content-right-product-button">
+                            <button type="submit">
+                                <i class="ti-shopping-cart" name="add-to-cart"></i>
+                                <p>Thue</p>
+                                
+                            </button>
+                        </div>
+                    </form>
+                   
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+@extends ('footer')
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use App\Http\Controllers\ArtistController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
