384cc33952d7ba5a54b7e56eac61aae2db55fdb8
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2023-04-24 10:51:34 +0700

    create contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+@extends('header')
+<h2 class="row-heading">CONTACTS</h2>
+<div class="row">
+    <div class="col col-half khoangcachdong">
+        <h2 class="heading">Manage Aritst</h2>
+        <p><i class="ti-location-pin"></i>118 Nguyen Khanh Toan - Cau Giay - Ha Noi</p>
+        <p><i class="ti-mobile"></i>+84 354676215</p>
+        <p><i class="ti-email"></i>truonganhtu190303@gmail.com</p>
+        <p><i class="ti-email"></i>leminhhoang@gmail.com</p>
+        <p><i class="ti-email"></i>vuthanhdat@gmail.com</p>
+    </div>
+@extends('header')
